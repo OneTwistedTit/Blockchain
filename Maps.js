ff19ea0ca98bfc7a7005bee25d180742ae29bdc3
OneTwistedTit <onetwistedtit@gmail.com>
2019-04-07 15:43:39 -0500

    Github Pages site

--- Maps.js
+++ Maps.js
@@ -19,7 +19,7 @@ var Start_Screen = {
 	"background": new Rectangle(0, 0, 1080, 768, "rgb(138, 0, 255"),
 	"credits": new Text(540, 768 - 15, "Created by John Doe 2019", "15px Comic Sans MS"),
 	"titleBG": new Rectangle(120, 35, 796 + 50, 120 + 20, "cyan"),
-	"title": new Text(540, 140, "WEENUSMAN", "120px Comic Sans MS"),
+	"title": new Text(540, 140, "LOREM IPSU", "120px Comic Sans MS"),
 	"playButton": new Rectangle(340, 334, 400, 200, "green"),
 	"playText": new Text(540, 474, "PLAY", "120px Comic Sans MS"),
 	"stopButton": new Circle(0, 0, 50, "red"),
