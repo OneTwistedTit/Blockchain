b8b868b7a3cf8df45f21c22bb40fb661b039f4dd
OneTwistedTit <onetwistedtit@gmail.com>
2019-04-06 21:27:34 -0500

    Concatenated into Objects.js

--- Objects.js
+++ Objects.js
@@ -1,3 +1,56 @@
+Vector2 = function(x, y){
+	if (x == null || y == null){
+		var err = "Missing parameters for Vector2: ";
+		if(x == null) err += "'x' ";
+		if(y == null) err += "'y'";
+		throw new Error(err);
+	}
+	this.x = x;
+	this.y = y;
+
+	this.prevX = 0;
+	this.prevY = 0;
+
+	this.Set = function(x, y){
+		if(x == null || y == null){
+			var err = "Missing parameters for Vector2.Set: ";
+			if(x == null) err += "'x' ";
+			if(y == null) err += "'y'";
+			throw new Error(err);
+		}
+		this.prevX = this.x;
+		this.prevY = this.y;
+
+		this.x = x;
+		this.y = y;
+	}
+	this.Normalize = function(){
+		var tmp = new Vector2(this.x, this.y);
+
+		var mag = Math.sqrt(tmp.x ** 2 + tmp.y ** 2);
+
+		tmp.x = tmp.x / mag;
+		tmp.y = tmp.y / mag;
+
+		return tmp;
+	}
+	this.Distance = function(vec2){
+		if(vec2 != null){
+			return Math.sqrt((vec2.x - this.x) ** 2 + (vec2.y - this.y) ** 2);
+		} else {
+			return Math.sqrt((this.prevX - this.x) ** 2 + (this.prevY - this.y) ** 2);
+		}
+	}
+	this.HasChanged = function(){
+		if(this.x != this.prevX || this.y != this.prevY) return true;
+		return false;
+	}
+	this.Difference = function(vec2){
+		if(vec2 != null) return new Vector2(this.x - this.prevX, this.y - this.prevY);
+		return new Vector2(this.x - vec2.x, this.y - vec2.y);
+	}
+}
+
 Rectangle = function(x, y, width, height){
 	if(x == null || y == null || width == null || height == null){
 		var err = "Missing parameters for Rectangle:";
--- main.js
+++ main.js
@@ -2,10 +2,6 @@
 var Player;
 var Elements = [];
 
-//Change the speed of the interval function
-//Base interval is 100
-
-
 function StartGame() {
   Game.Start();
   Player = new Rectangle(50, 50, 50, 50);
@@ -14,15 +10,17 @@ function StartGame() {
 }
 function UpdateGame(){
   var prevPos = new Vector2(Player.x, Player.y);
-  
   Game.Clear();
   Game.frame++;
+  var direction = new Vector2(0, 0);
   if (Game.keys) {
-    if (Game.keys[65]) Player.x += -1;
-    if (Game.keys[68]) Player.x += 1;
-    if (Game.keys[87]) Player.y += -1;
-    if (Game.keys[83]) Player.y += 1;
+    if (Game.keys[65]) direction.Set(-1, direction.y);
+    if (Game.keys[68]) direction.Set(1, direction.y);
+    if (Game.keys[87]) direction.Set(direction.x, -1);
+    if (Game.keys[83]) direction.Set(direction.x, 1);
   }
+  Player.x = prevPos.x + direction.x;
+  Player.y = prevPos.y + direction.y;
   if (Player.x < 0){
     Player.x = 0;
   }
@@ -42,30 +40,7 @@ function UpdateGame(){
         Player.y = prevPos.y;
     }
   }
-  Elements[0].Draw(Game.context, "red");
-  //document.getElementById('debug').innerText = `Entities: ${Elements.length}; X, Y: ${Player.x}, ${Player.y} `
+  //document.getElementById('debug').innerText = ``
   Player.Draw(Game.context, "blue");
 }
 
-
-
-//Updates the graphics and the data of the Game elements
-/*function updateGame() {
-
-  document.getElementById('debug').innerText = `Entities: ${Elements.length}; X, Y: ${Player.x}, ${Player.y} `
-
-  //Binds the Player to the Game board
-  if (Player.x < 0){
-    Player.x = 0;
-  }
-  if (Player.y < 0){
-    Player.y = 0;
-  }
-  if (Player.x > Game.canvas.width - Player.width){
-    Player.x = Game.canvas.width - Player.width;
-  }
-  if (Player.y > Game.canvas.height - Player.height){
-    Player.y = Game.canvas.height - Player.height;
-  }
-  Player.update();
-}*/
