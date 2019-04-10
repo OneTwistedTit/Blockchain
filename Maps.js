4d5e3b550884446a7f439d67d751a47ce16456b0
OneTwistedTit <onetwistedtit@gmail.com>
2019-04-09 19:24:40 -0500

    'Grabbing scoreless WormThumb'

--- Maps.js
+++ Maps.js
@@ -69,9 +69,11 @@ var Game_Selection = {
 					Game.Load(Start_Screen);
 				}
 			}
-			if(Game.Elements["resetButton"].Contains(Game.click)){
-				Game.click.Set(null, null);
-				Game.Reset();
+			if(Game.Elements["resetButton"]){
+				if(Game.Elements["resetButton"].Contains(Game.click)){
+					Game.click.Set(null, null);
+					Game.Reset();
+				}
 			}
 		}
 	},
@@ -140,7 +142,7 @@ var WormThumb = {
 		console.log("Loaded WormThumb");
 	},
 	Update: function(){
-		if(Game.Elements["resetButton"].Contains(Game.click)){
+		/*if(Game.Elements["resetButton"].Contains(Game.click)){
 			Game.click.Set(null, null);
 			Game.Reset();
 		}
@@ -148,7 +150,7 @@ var WormThumb = {
 			Game.click.Set(null, null);
 			console.log("Moving to Game Selection");
 			Game.Load(Game_Selection);
-		}
+		}*/
 	},
 	"background": new Rectangle(0, 0, 1080, 768, "white"),
 	"border": {
@@ -160,12 +162,12 @@ var WormThumb = {
 	"bottomSegment": new Rectangle(65, 655, 200, 25, "green"),
 	"middleSegment": new Rectangle(65, 455, 25, 200, "green"),
 	"topSegment": new Rectangle(65, 430, 250, 25, "green"),
-	"apple": new Rectangle(365, 430, 25, 25, "red"),
-	"length": new Text(540, 105 - 30, "Length: 26", "60px Comic Sans MS"),
+	"apple": new Rectangle(365, 430, 25, 25, "red")
+	/*"length": new Text(540, 105 - 30, "Length: 26", "60px Comic Sans MS"),
 	"backButton": new Rectangle(780, 0, 315, 75, "cyan"),
 	"backText": new Text(780 + 150, 60, "<- BACK", "60px Comic Sans MS"),
 	"resetButton": new Circle(0, 0, 50, "red"),
-	"resetText": new Text(50, 100 - 35, "RESET", "30px Comic Sans MS")
+	"resetText": new Text(50, 100 - 35, "RESET", "30px Comic Sans MS")*/
 }
 
 var BarrelRollerThumb = {
@@ -220,6 +222,8 @@ var BarrelRollerThumb = {
 var WormGame = {
 	Start: function(){
 		console.log("Loaded Worm");
+		var velocity = new Vector2(1, 0),
+			length			
 	},
 	Update: function(){
 
@@ -231,5 +235,164 @@ var WormGame = {
 			Game.context.strokeRect(12, 105, 1055, 655);
 		}
 	},
+	/*
+
+
+	var vel = [1, 0],
+		notStarted = true,
+		t = 300,
+		lemon = null,
+		timeToLemon = 1,
+		points = 0,
+		gridSize = 20,
+		body = [[(gridSize/2|0)-1, (gridSize/2|0)-1]],
+		lastMoved = [],
+		highScore = Number(localStorage.getItem("highScore")) || 0,
+		canvas = document.getElementById("c"),
+		squareSize = canvas.width / gridSize,
+		ctx = canvas.getContext("2d");
+
+	var ptingImg = new Image();
+	ptingImg.src = "pting.png";
+
+	var lemonImg = new Image();
+	lemonImg.src = "lemon.png";
+
+
+	addEventListener("keydown", function(e){
+		startGame();
+
+		switch(e.key){
+			case "w":
+			case "ArrowUp":
+				changeVel([0, -1]);
+				break;
+			case "d":
+			case "ArrowRight":
+				changeVel([1, 0]);
+				break;
+			case "s":
+			case "ArrowDown":
+				changeVel([0, 1]);
+				break;
+			case "a":
+			case "ArrowLeft":
+				changeVel([-1, 0]);
+				break;
+
+			case "m":
+				music.playing() ? music.stop() : music.play();
+				localStorage.setItem("mute", music.playing() ? "" : "1");
+				break;
+		}
+	});
+
+	canvas.addEventListener("click", function(e){
+		startGame();
+
+		e.preventDefault();
+		let bounds = canvas.getBoundingClientRect();
+		let x = (e.clientX - bounds.x) - canvas.width/2,
+			y = (e.clientY - bounds.y) - canvas.height/2;
+
+		if(y > Math.abs(x)){
+			changeVel([0, 1]);
+		} else if(-y > Math.abs(x)){
+			changeVel([0, -1]);
+		} else if(x > Math.abs(y)){
+			changeVel([1, 0]);
+		} else if(-x > Math.abs(y)){
+			changeVel([-1, 0]);
+		}
+	});
+
+	function startGame(){
+		if(notStarted){
+			if(!localStorage.getItem("mute"))
+				music.play();
+			notStarted = false;
+			gameLoop();
+		}
+	}
+
+
+	function changeVel(v){
+		if(lastMoved[1-v.indexOf(0)] != -v[1-v.indexOf(0)] || body.length == 1)
+			vel = v;
+	}
+
+	function gameOver(m){
+		if(points > highScore)
+			localStorage.setItem("highScore", points.toString());
+
+		music.stop();
+		(new Audio("rarg.mp3")).play().then(function(){
+			alert(m + "\n\npting has collected " + points + " lemon" + (points == 1 ? "" : "s"));
+			location.reload();
+		});
+	}
+
+	function draw(){
+		ctx.fillStyle = "#222";
+		ctx.fillRect(0, 0, canvas.width, canvas.width);
+
+		if(lemon)
+			ctx.drawImage(lemonImg, lemon[0] * squareSize, lemon[1] * squareSize, squareSize, squareSize);
+
+		for(var b of body)
+			ctx.drawImage(ptingImg, b[0] * squareSize, b[1] * squareSize, squareSize, squareSize);
+
+		ctx.fillStyle = "#fff";
+		ctx.font = "20px Georgia";
+		ctx.fillText("lemons: " + points, 3, 23);
+		ctx.fillText("highscore: " + highScore, 3, 46);
+	}
+
+	function gameLoop(){
+		let n = [].concat(body[body.length - 1]);
+
+		n[0] += vel[0];
+		n[1] += vel[1];
+
+		if(n[0] >= gridSize || n[0] < 0 || n[1] >= gridSize || n[1] < 0)
+			return gameOver("you ran into a wall and binged your head :(");
+
+		for(var b of body){
+			if(b[0] == n[0] && b[1] == n[1])
+				return gameOver("you have crashed into one of your fellow ptings, causing a quite a dispute :(");
+		}
+
+		if(timeToLemon <= 0 && !lemon){
+			timeToLemon = 10;
+			lemon = [(Math.random() * gridSize)|0, (Math.random() * gridSize)|0];
+			t = Math.max(t - (t/20)|0, 100);
+		} else
+			timeToLemon--;
+
+		if(lemon)
+			if(body[body.length-1][0] == lemon[0] && body[body.length-1][1] == lemon[1]){
+				lemon = null;
+				points += 1;
+				(new Audio("munch.mp3")).play();
+
+				body = [body[0]].concat(body);
+			}
+
+		lastMoved = vel;
+
+		body.push(n);
+		body = body.slice(1);
+
+		draw();
+		setTimeout(function(){
+			gameLoop();
+		}, t);
+	}
+	
+	draw();
+
+	*/
 	"gameArea": new Rectangle(15, 108, 1050, 650)
+	//"score": new Text(540, 78, "Length: 1")
+
 }
